fff7c0166dafe43b54fde32aab169866bdca93de
Johnnnyyyy <real.johnreyalcantara@gmail.com>
2026-03-04 14:41:42 +0800

    new changes 3

--- types.d.ts
+++ types.d.ts
@@ -1,1 +1,16 @@
 import '@react-three/fiber';
+import { BoxGeometryProps, MeshProps, MeshStandardMaterialProps, PrimitiveProps, SphereGeometryProps } from '@react-three/fiber';
+import { Group, Object3D } from 'three';
+
+declare module 'react' {
+  namespace JSX {
+    interface IntrinsicElements {
+      mesh: MeshProps & { children?: React.ReactNode };
+      sphereGeometry: SphereGeometryProps;
+      boxGeometry: BoxGeometryProps;
+      meshStandardMaterial: MeshStandardMaterialProps;
+      group: { children?: React.ReactNode } & Partial<Group>;
+      primitive: PrimitiveProps<Object3D>;
+    }
+  }
+}
