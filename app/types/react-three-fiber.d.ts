cca9e18378b62badf6665d58f4d12ce6acf4526d
Johnnnyyyy <real.johnreyalcantara@gmail.com>
2026-03-04 11:44:28 +0800

    Changes

---
+++ app/types/react-three-fiber.d.ts
@@ -0,0 +1,15 @@
+// types/react-three-fiber.d.ts
+import { ReactThreeFiber } from "@react-three/fiber";
+import * as THREE from "three";
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      ambientLight: ReactThreeFiber.Object3DNode<THREE.AmbientLight, typeof THREE.AmbientLight>;
+      directionalLight: ReactThreeFiber.Object3DNode<THREE.DirectionalLight, typeof THREE.DirectionalLight>;
+      pointLight: ReactThreeFiber.Object3DNode<THREE.PointLight, typeof THREE.PointLight>;
+      spotLight: ReactThreeFiber.Object3DNode<THREE.SpotLight, typeof THREE.SpotLight>;
+      // add other lights or meshes as needed
+    }
+  }
+}
